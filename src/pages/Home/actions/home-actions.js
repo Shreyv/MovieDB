450269d91eedab77d73209fa9687b225ef684a5c
Shrey <shreyv991995@gmail.com>
2019-02-16 23:13:28 +0530

    initial commit

---
+++ src/pages/Home/actions/home-actions.js
@@ -0,0 +1,37 @@
+import ActionTypes from "../../../actions";
+import {buildUrl} from "../../../Utils/Url";
+import constants from "../../../constants";
+
+
+
+const Actions = {
+    _loadDefault: (page) => async (dispatch) => {
+        try {
+            const params = {
+                language: "en-US",
+                sort_by: "popularity.desc",
+                include_adult: false,
+                include_video: false,
+                page: page
+            };
+            dispatch({ type: ActionTypes.HOME.HOME_LOAD_DEFAULT })      
+            const url = buildUrl(constants.API_KEY, constants.API_BASE_URL, "discover/movie", params);
+            const response = await fetch(url);
+            const json = await response.json();
+            dispatch({
+                type: ActionTypes.HOME.HOME_LOAD_DEFAULT_SUCCESS,
+                page: json.page,
+                results: json.results,
+                total_pages: json.total_pages,
+                total_results: json.total_results
+            });
+        } catch(error) {
+            dispatch({ type: ActionTypes.HOME.HOME_LOAD_DEFAULT_FAILURE });
+        }
+    },
+    _setPage: (page) => async (dispatch) => {
+        dispatch({ type: ActionTypes.HOME.HOME_SET_PAGE_NUMBER, page });
+    }
+};
+
+export default Actions;
